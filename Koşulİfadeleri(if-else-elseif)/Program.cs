51d39624064cd91958d96b01e35b4883b3154e4c
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 09:39:09 +0300

    C# Kosullu ifadeler yapıldı.

--- Koşulİfadeleri(if-else-elseif)/Program.cs
+++ Koşulİfadeleri(if-else-elseif)/Program.cs
@@ -10,6 +10,24 @@ namespace Koşulİfadeleri_if_else_elseif_
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Merhaba");
+
+            Console.Write("Sayı 1 degeri giriniz : ");
+            decimal sayi1 = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Sayı 2 degerini giriniz : ");
+            decimal sayi2 = Convert.ToDecimal(Console.ReadLine());
+
+            decimal toplam = sayi1 + sayi2;
+
+            if(toplam > 50)
+            {
+                Console.WriteLine("Girmiş olduğunuz değerlerin toplamı 50 den büyüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Girmiş oldugunuz değerlerin toplamı 50 den küçüktür.");
+            }
         }
     }
 }
