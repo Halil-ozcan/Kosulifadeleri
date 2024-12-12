e7c6752f3c754c09839bd0948fce69766b81f99e
Halil Özcan <halil_ozcann@outlook.com>
2024-12-12 11:20:12 +0300

    C# Karar Yapilari Alistirma 5 Yapıldı

---
+++ KararYapilariAlistirma5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KararYapilariAlistirma5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kullanıcı Kodu");
+            string KullaniciKodu = Console.ReadLine();
+
+            switch(KullaniciKodu)
+            {
+                case "AA":
+                    Console.WriteLine("Admin Yetkisine Sahipsiniz");
+                    break;
+                case "BB":
+                    Console.WriteLine("Admin Yetkisine Sahipsiniz");
+                    break;
+                case "CC":
+                    Console.WriteLine("Admin Yetkisine Sahipsiniz");
+                    break;
+                case "DD":
+                    Console.WriteLine(" Güçlü Yetkisine Sahipsiniz");
+                    break;
+                case "FF":
+                    Console.WriteLine("Standart Yetkisine Sahipsiniz");
+                    break;
+                case "EE":
+                    Console.WriteLine("Standart Yetkisine Sahipsiniz");
+                    break;
+                case "GG":
+                    Console.WriteLine("Standart Yetkisine Sahipsiniz");
+                    break;
+                default:
+                    Console.WriteLine("Hatalı Kullanıcı Kodu");
+                    break;
+            }
+
+        }
+    }
+}
